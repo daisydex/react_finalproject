f71a668895d9c1e78b91cad333039febf91f5b30
daisydex <daisy.congiu@gmail.com>
2021-03-15 19:11:30 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    Weather App  
+    <h1>Weather App </h1>
+    <footer>
+      This project was coded by Daisy and is {""}
+    <a href="https://github.com/daisydex/react_finalproject" target="_blank"> open-sourced on GitHub </a>
+    </footer>
     </div>
   );
 }
