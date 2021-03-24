5622dc98b2f698e3637d4f216ab6a30afb2e213a
daisydex <daisy.congiu@gmail.com>
2021-03-24 09:10:19 +0100

    Revert "Removed unnecessary files"
    This reverts commit d9662c1cad54f223e9ccb87b58941182e2686800.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,15 +18,16 @@ export default function WeatherInfo (props){
         <div className="col-6">
           <div className="clearfix">
               <div className="float-left">
-          
+               <div className="mainIcon">
               <WeatherIcon code={props.data.icon} />
-        
+          </div> 
             </div>
             </div>
             <div className="clearfix">
             <div className="float-left">
    
             <WeatherTemperature celsius={props.data.temperature}/>
+         
 
             </div>
            </div>
